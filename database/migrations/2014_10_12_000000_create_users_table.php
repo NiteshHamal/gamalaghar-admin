ab1241a24b8b44517aa2dab3e1f112250e1552a3
NiteshHamal <nitesh0hamal@gmail.com>
2024-01-28 11:40:51 +0545

    user schema created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,18 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('type');
+            $table->integer('phone');
+            $table->string('company_name');
+            $table->string('tax_nr');
+            $table->string('registration_nr');
+            $table->string('currency');
+            $table->string('timezone');
+            $table->tinyInteger('is_active');
+            $table->decimal('Itv');
+            $table->tinyInteger('newsletter');
+            $table->dateTime('last_purchased_at');
+            $table->dateTime('last_login_at');
             $table->rememberToken();
             $table->timestamps();
         });
