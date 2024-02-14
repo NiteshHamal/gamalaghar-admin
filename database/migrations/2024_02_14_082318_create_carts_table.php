bfa723804d5620046528f39be04dc95e18c9c032
NiteshHamal <nitesh0hamal@gmail.com>
2024-02-14 08:31:18 +0000

    commit change

--- database/migrations/2024_02_14_082318_create_carts_table.php
+++ database/migrations/2024_02_14_082318_create_carts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->string('user_id');
+            $table->string('product_id');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
