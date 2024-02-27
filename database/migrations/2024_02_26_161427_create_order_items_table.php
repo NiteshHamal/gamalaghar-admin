971f891f2e098608cc10461ccd27a1811ceaae8f
NiteshHamal <nitesh0hamal@gmail.com>
2024-02-27 14:02:27 +0545

    commit change

--- database/migrations/2024_02_26_161427_create_order_items_table.php
+++ database/migrations/2024_02_26_161427_create_order_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->nullable();
             $table->foreignId('order_id')->nullable();
             $table->foreignId('product_id')->nullable();
             $table->string('product_name')->nullable();
