ecd3fcf454e29ef01a4ce34b6a5fbb2a7e1923f1
Hancie123 <hanciewanemphago@gmail.com>
2024-03-10 16:37:52 +0545

    search added on product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -58,6 +58,13 @@ class ProductController extends Controller
     {
         try {
             $product = DB::transaction(function () use ($request) {
+
+                $size = $request->input('size');
+                $price = $request->input('price');
+                $product_stock = $request->input('product_stock');
+                $highestPrice = max($price);
+
+                
                 $product = Product::create([
                     'user_id' => auth()->user()->id,
                     'product_name' => $request->product_name,
@@ -65,28 +72,28 @@ class ProductController extends Controller
                     'short_description' => $request->short_description,
                     'description' => $request->description,
                     'discount' => $request->discount,
-                    'product_code' => $request->product_code
+                    'product_code' => $request->product_code,
+                    'product_price'=>$highestPrice,
 
                 ]);
 
-                $size = $request->input('size');
-                $price = $request->input('price');
-                $product_stock = $request->input('product_stock');
+                
 
                 // Loop through the arrays and save each product
-                for (
-                    $i = 0;
-                    $i < count($size);
-                    $i++
-                ) {
-                    $productSizePrice = new ProductSizePrice();
-                    $productSizePrice->size_id = $size[$i];
-                    $productSizePrice->product_id = $product->id;
-                    $productSizePrice->price = $price[$i];
-                    $productSizePrice->product_stock = $product_stock[$i];
-                    $productSizePrice->save();
+                if (!empty($size) && !empty($price) && !empty($product_stock) && count($size) === count($price) && count($price) === count($product_stock)) {
+                    // Loop through the arrays and save each product
+                    for ($i = 0; $i < count($size); $i++) {
+                        // Check if any of the values are null or empty strings, if so, skip this iteration
+                        if ($size[$i] !== null && $price[$i] !== null && $product_stock[$i] !== null && $size[$i] !== '' && $price[$i] !== '' && $product_stock[$i] !== '') {
+                            $productSizePrice = new ProductSizePrice();
+                            $productSizePrice->size_id = $size[$i];
+                            $productSizePrice->product_id = $product->id;
+                            $productSizePrice->price = $price[$i];
+                            $productSizePrice->product_stock = $product_stock[$i];
+                            $productSizePrice->save();
+                        }
+                    }
                 }
-
                 if ($request->product_image) {
                     $product->addMedia($request->product_image)->toMediaCollection('product_image');
                 }
